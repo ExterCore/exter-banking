fd1919d3ca39d8643a1117ef2d015a20b44df025
sobing4413 <95325881+sobing4413@users.noreply.github.com>
2024-10-12 11:15:24 +0700

    Add files via upload

---
+++ Client/main.lua
@@ -0,0 +1,29 @@
+Exter = exports["exter_core"]:getSharedObject()
+
+RegisterNetEvent("exter-banking:Client:BankMenu:Show")
+AddEventHandler("exter-banking:Client:BankMenu:Show", function() 
+    Exter.TriggerServerCallback("exter-banking:Server:GetUserAccounts", {}, function(bankData) 
+        if bankData then 
+            SetNuiFocus(true, true)
+
+            TriggerScreenblurFadeIn(500)
+            
+            SendNUIMessage({
+                action = "showMenu",
+                playerName = bankData.playerName,
+                accounts = bankData.accounts
+            })
+        end
+    end)
+end)
+
+RegisterNetEvent("exter-banking:Client:BankMenu:Hide")
+AddEventHandler("exter-banking:Client:BankMenu:Hide", function() 
+    SetNuiFocus(false, false)
+    
+    TriggerScreenblurFadeOut(500)
+
+    SendNUIMessage({
+        action = "hideMenu"
+    })
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'adamant'
+lua54 'yes'
+game 'gta5'
+
+author 'sobing'
+
+description 'exter-banking script | NP 4.0 Inspired'
+
+version '1.0'
+
+ui_page 'UI/index.html'
+
+shared_script 'Config.lua'
+
+client_script 'Client/*.lua'
+
+server_script 'Server/*.lua'
+
+files {
+    'UI/index.html',
+    'UI/styles/*.css',
+    'UI/scripts/*.js',
+    'UI/images/*.png',
+	'UI/images/*.jpg',
+    'UI/images/*.svg',
+	'UI/fonts/*.ttf'
+}
